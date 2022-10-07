8aaf87e3956020e1de3f7b4e3a7d755173bf0adb
Shoxbozbek <shoxbozbek9798@gmail.com>
2022-10-07 13:46:58 +0300

    main

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('krosovka-api/<int:pk>/', singleAPI),
     path('create/', malumotjoylash),
     path('create/<int:pk>/', malumotUpdate),
-    path('delete/', malumotDelete),
+    path('delete/<int:pk>/', malumotDelete),
 ]
